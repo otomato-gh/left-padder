92b6663786dd7b8b69d85fe424bb5338f2af173a
Ant Weiss <ant.weiss@gmail.com>
2016-04-20 15:26:09 +0300

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+
+/*
+ * GET home page.
+ */
+cflp = require('cf-left-pad');
+var results = ["none"];
+
+exports.index = function(req, res){
+  console.log(results);
+  res.render( 'index', {
+        title : 'LeftPad',
+        results : results,
+    });
+};
+
+exports.leftpad = function ( req, res ){
+    console.log(req.body);
+    results.push(cflp(req.body.string2pad, req.body.len2pad, req.body.char2pad));
+    res.redirect( '/' );
+};
