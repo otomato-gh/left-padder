df227d40b6547d918db5439baf2ca8120a72f9ca
Ant Weiss <ant.weiss@gmail.com>
2016-04-23 19:40:38 +0300

    a small change

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,6 @@
 cflp = require('cf-left-pad');
 
 exports.leftpad = function ( req, res ){
-    console.log(req.body);
+    console.log("request:"+req.body);
     res.send(cflp(req.body.string2pad, req.body.len2pad, req.body.char2pad));
 };
